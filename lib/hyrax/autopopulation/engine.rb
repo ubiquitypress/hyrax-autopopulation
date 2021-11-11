09c8c3d171ad8c28b38abe0e1e3bf68b20a5c820
edward <bauchiroad@gmail.com>
2021-11-11 15:09:37 +0000

    add rspec, rubocop, circleci

--- lib/hyrax/autopopulation/engine.rb
+++ lib/hyrax/autopopulation/engine.rb
@@ -1,7 +1,14 @@
+# frozen_string_literal: true
 module Hyrax
   module Autopopulation
     class Engine < ::Rails::Engine
       isolate_namespace Hyrax::Autopopulation
+
+      config.generators do |g|
+        g.test_framework :rspec
+        g.fixture_replacement :factory_bot
+        g.factory_bot dir: 'spec/factories'
+      end
     end
   end
 end
