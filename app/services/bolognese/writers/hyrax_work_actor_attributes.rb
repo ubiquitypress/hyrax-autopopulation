e67a3b4981a3caeaddba5df0e43043ad6a7bdd09
DanukaH <danuka.hettiarachchi@ubiquitypress.com>
2023-09-08 04:19:53 +0100

    REPO-2161 - Fix method call

--- app/services/bolognese/writers/hyrax_work_actor_attributes.rb
+++ app/services/bolognese/writers/hyrax_work_actor_attributes.rb
@@ -16,15 +16,19 @@ module Bolognese
         }
       end
 
+      def crossref_type
+        @crossref_type = meta["types"].dig("resourceType").underscore
+      end
+
       def map_work_type
-        crossref_type = meta["types"].dig("resourceType").underscore
-        crossref_hyku_mappings = Site.account.settings&.dig("crossref_hyku_mappings")
+        # @crossref_type = meta["types"].dig("resourceType").underscore
+        @crossref_hyku_mappings = Site.account.settings&.dig("crossref_hyku_mappings")
 
-        puts "LOG_crossref_type #{crossref_type.inspect}"
-        puts "LOG_crossref_hyku_mappings #{crossref_hyku_mappings.inspect}"
+        puts "LOG_crossref_type #{@crossref_type.inspect}"
+        puts "LOG_crossref_hyku_mappings #{@crossref_hyku_mappings.inspect}"
 
-        if crossref_hyku_mappings.key?(crossref_type)
-          klass_name = crossref_hyku_mappings[crossref_type]
+        if @crossref_hyku_mappings.key?(@crossref_type)
+          klass_name = @crossref_hyku_mappings[@crossref_type]
           puts "LOG_klass_name #{klass_name.inspect}"
           @mapped_work_type = klass_name if class_exists?(klass_name)
         end
