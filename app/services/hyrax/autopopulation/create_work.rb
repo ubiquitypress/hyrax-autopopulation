96d96dd744cd49b4af203929bb9bf80ff0d3b509
DanukaH <danuka.hettiarachchi@ubiquitypress.com>
2023-09-12 20:23:27 +0100

    REPO-2161 - Fixing conversion issue

--- app/services/hyrax/autopopulation/create_work.rb
+++ app/services/hyrax/autopopulation/create_work.rb
@@ -48,14 +48,14 @@ module Hyrax
 
         def actor_environment
           klass = Hyrax::Actors::Environment
-          puts "LOG_ATTRIBUTES_actor_env #{@attributes.inspect}"
+          # puts "LOG_ATTRIBUTES_actor_env #{@attributes.inspect}"
           crossref_types = fetch_crossref_types(@attributes[:doi])
-          puts "LOG_crossref_work_type_create_work_53 #{crossref_types.inspect}"
-          crossref_work_type = crossref_types[:types].dig("resourceType")&.underscore
-          puts "LOG_crossref_work_type_create_work_55 #{crossref_work_type.inspect}"
+          # puts "LOG_crossref_work_type_create_work_53 #{crossref_types.inspect}"
+          crossref_work_type = crossref_types[:types].pluck("resourceType")&.underscore
+          # puts "LOG_crossref_work_type_create_work_55 #{crossref_work_type.inspect}"
 
           @mapped_work_type = map_work_type(crossref_work_type)
-          puts "LOG_@mapped_work_type_59 #{@mapped_work_type.inspect}"
+          # puts "LOG_@mapped_work_type_59 #{@mapped_work_type.inspect}"
           @_actor_environment ||= if Rails.application.config.hyrax_autopopulation.active_record?
                                     klass.new(Object.const_get(@mapped_work_type).new, ::Ability.new(user), attributes)
                                   else
