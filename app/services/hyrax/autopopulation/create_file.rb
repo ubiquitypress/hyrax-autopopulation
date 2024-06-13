f19e0c58fd97c6ed9ffd655e88f05571bc3c79b8
DanukaH <danuka.hettiarachchi@ubiquitypress.com>
2024-06-13 11:19:09 +0100

    REPO-2346 - Truncating url before filename assignment.

--- app/services/hyrax/autopopulation/create_file.rb
+++ app/services/hyrax/autopopulation/create_file.rb
@@ -14,17 +14,17 @@ module Hyrax
 
         return unless @url.present?
 
-        Rails.logger.info "LOG_URL passed to CreateFile initializer: #{@url}"
-        @filename = File.basename(url)
-        # @filename = File.basename(URI.parse(url).path)
-        Rails.logger.info "LOG_Filename after extraction: #{@filename}"
+        Rails.logger.info "LOG_url before truncation: #{url}"
 
-        if @filename.length > 255
-          hashed_part = Digest::SHA256.hexdigest(@filename)[0, 10] # take first 10 char of hash
-          @filename = "#{hashed_part}-#{@filename[0, 244]}" # truncate original name and append hash
+        if url.length > 255
+          hashed_part = Digest::SHA256.hexdigest(url)[0, 10] # take first 10 char of hash
+          url = "#{hashed_part}-#{url[0, 200]}" # truncate original name and append hash
         end
 
-        Rails.logger.info "LOG_Filename after truncation: #{@filename}"
+        Rails.logger.info "LOG_url after truncation: #{url}"
+
+        @filename = File.basename(url)
+        # @filename = File.basename(URI.parse(url).path)
 
         @user = user
         @account = account
