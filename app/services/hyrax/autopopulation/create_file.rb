996e1f5896c15fb2a486ffe7147ee9351a60e892
DanukaH <danuka.hettiarachchi@ubiquitypress.com>
2024-06-18 13:35:56 +0100

    REPO-2024 - debugging the process

--- app/services/hyrax/autopopulation/create_file.rb
+++ app/services/hyrax/autopopulation/create_file.rb
@@ -43,10 +43,14 @@ module Hyrax
           end
         end
 
+        Rails.logger.info "LOG_File.extname(filename).empty?: #{File.extname(filename).empty?}"
+        Rails.logger.info "LOG_File.extname(filename): #{File.extname(filename)}"
         # Add .pdf extension if filename does not have an extension
         if File.extname(filename).empty?
+          Rails.logger.info "LOG_inside the if condition"
           filename += ".pdf"
         end
+        Rails.logger.info "LOG_filename: #{filename}"
 
         @filename = filename
       end
